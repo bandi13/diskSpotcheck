f2e2183f4950abf619fe36f60798f5aecf9bf3dd
Andras Fekete <bandi13@users.noreply.github.com>
2016-05-14 16:56:47 -0400

    Quick commit to make more options on the utility. Largely untested, but I believe in myself. :-)

--- diskSpotcheck.cpp
+++ diskSpotcheck.cpp
@@ -43,64 +43,72 @@ static void dropSystemCache() {
 	ofs.close();
 }
 
-#define MYBUFSIZ 5000
 #define LOCCNT 1000
 #define NUMPASSES 3
-double doPass(int fd, char c, uint64_t maxLoc) {
-	std::unique_ptr<char[]> raiiBuf = std::make_unique<char[]>(MYBUFSIZ);
+double doPass(int fd, char c, uint64_t maxLoc, size_t bufSize) {
+	std::unique_ptr<char[]> raiiBuf = std::make_unique<char[]>(bufSize);
 	char *buf = raiiBuf.get();
 	uint64_t locs[LOCCNT];
 
 	dropSystemCache();
 	srand(c);
-	for(int i = 0; i < MYBUFSIZ; i++) buf[i] = rand();
+	for(uint64_t i = 0; i < bufSize; i++) buf[i] = rand();
 	cout << "Starting test of char=" << c << endl;
-	maxLoc -= MYBUFSIZ; // make sure we don't accidentally try to write off the end of the file
+	maxLoc -= bufSize; // make sure we don't accidentally try to write off the end of the file
 	locs[0] = 0; // make sure we get the beginning
 	locs[LOCCNT - 1] = maxLoc; // make sure we get the end
-	for(int i = 1; i < LOCCNT - 1; i++) {
-		locs[i] = (((float)rand() / RAND_MAX) * (maxLoc - locs[i-1] - MYBUFSIZ)) / ((LOCCNT - 2)/4) + locs[i-1] + MYBUFSIZ; // set up the location to be written relative to the last one
+	for(uint64_t i = 1; i < LOCCNT - 1; i++) {
+		locs[i] = (((float)rand() / RAND_MAX) * (maxLoc - locs[i-1] - bufSize)) / ((LOCCNT - 2)/4) + locs[i-1] + bufSize; // set up the location to be written relative to the last one
 	}
 	auto startT = std::chrono::steady_clock::now();
-	for(int i = 0; i < LOCCNT; i++) {
+	for(uint64_t i = 0; i < LOCCNT; i++) {
 		//		cout << i << ": Writing to " << locs[i] << endl;
 		lseek64(fd,locs[i],SEEK_SET);
-		if(write(fd,buf,MYBUFSIZ) != MYBUFSIZ) { cerr << "Didn't complete a write of " << MYBUFSIZ << " * '" << c << "' at " << locs[i] << " because " << strerror(errno) << endl; return -1; }
+		if(write(fd,buf,bufSize) != (int)bufSize) { cerr << "Didn't complete a write of " << bufSize << " * '" << c << "' at " << locs[i] << " because " << strerror(errno) << endl; return -1; }
 	}
 	if(syncfs(fd) == -1) { cerr << "Sync error: " << strerror(errno) << endl; return -3; }
 	dropSystemCache();
-	for(int i = 0; i < LOCCNT; i++) {
+	for(uint64_t i = 0; i < LOCCNT; i++) {
 		//		cout << i << ": Reading from " << locs[i] << endl;
 		lseek64(fd,locs[i],SEEK_SET);
-		if(read(fd,buf,MYBUFSIZ) != MYBUFSIZ) { cerr << "Didn't complete a read of " << MYBUFSIZ << " * '" << c << "' at " << locs[i] << " because " << strerror(errno) << endl; return -3; }
+		if(read(fd,buf,bufSize) != (int)bufSize) { cerr << "Didn't complete a read of " << bufSize << " * '" << c << "' at " << locs[i] << " because " << strerror(errno) << endl; return -3; }
 		srand(c);
 		char rng;
-		for(int j = 0; j < MYBUFSIZ; j++)
+		for(uint64_t j = 0; j < bufSize; j++)
 			if(buf[j] != (rng = rand())) {
 				cerr << "Verification of write/read failed at location " << locs[i] << ", offset=" << j << endl;
 				cerr << "  expected=";
-				for(int k = 0; k < MYBUFSIZ; k++) { cerr << (int)rng << ','; rng = rand(); }
+				for(uint64_t k = 0; k < bufSize; k++) { cerr << (int)rng << ','; rng = rand(); }
 				cerr << endl;
 				cerr << "       got=";
-				for(int k = 0; k < MYBUFSIZ; k++) cerr << (int)buf[k] << ',';
+				for(uint64_t k = 0; k < bufSize; k++) cerr << (int)buf[k] << ',';
 				cerr << endl;
 				return -4;
 			}
 	}
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds >(std::chrono::steady_clock::now() - startT).count() / 1000.0;
-	double speed = ((double)(MYBUFSIZ*LOCCNT)/duration)/(1024*1024);
+	double speed = ((double)(bufSize*LOCCNT)/duration)/(1024*1024);
 	cout << "Test completed in " << duration << " seconds. Speed=" << speed << "MB/s." << endl;
 	return speed;
 }
 
-#define doUsage(errStream) { cerr << errStream << endl << "Usage: " << argv[0] << " [<device> [<diskSize>]]" << endl; return -1; }
+#define doUsage(errStream) { cerr << errStream << endl << "Usage: " << argv[0] << " [-d <device>] [-s <diskSizeInMB>] [-b <bufSizeInKB>] [-h]" << endl; return -1; }
 int main(int argc, char *argv[]) {
 	int fd;
-	uint64_t diskSize = 0;
-	if(argc >= 2) fd = open(argv[1],O_RDWR|O_LARGEFILE);
-	else fd = open("/dev/nbd0",O_RDWR|O_LARGEFILE);
-	if(fd == -1) doUsage("Error opening file");
-	if(argc == 3) { diskSize = atol(argv[2]); cout << "Setting DiskSize=" << diskSize << endl; }
+	int opt;
+	size_t diskSize = 0;
+	size_t bufSize = 64*1024;
+	std::string diskPath = "/dev/nbd0";
+	while ((opt = getopt(argc, argv, "b:d:s:h")) != -1) {
+		switch (opt) {
+			case 'b': bufSize = (size_t)atoi(optarg) * 1024; break;
+			case 's': diskSize = (size_t)atoi(optarg) * 1024 * 1024; break;
+			case 'd': diskPath = optarg; break;
+			case 'h': doUsage("Help requested"); return -1;
+			default:  doUsage("Unknown argument"); return -1;
+		}
+	}
+	if((fd = open(diskPath.c_str(),O_RDWR|O_LARGEFILE)) == -1) doUsage("Error opening " << diskPath << ": " << strerror(errno));
 	if(diskSize == 0) {
 		struct stat fd_stat;
 		if(fstat(fd,&fd_stat)) doUsage("Error getting the stats on the file: " << strerror(errno));
@@ -111,12 +119,14 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	if(diskSize < MYBUFSIZ) doUsage( "DiskSize<"<<(uint64_t)MYBUFSIZ<<", we can't deal with that.");
-	cout << "Running test with diskSize=" << diskSize << endl;
+	cout << "Setting diskSize=" << diskSize << ", bufSize=" << bufSize << endl;
+
+	if(diskSize < bufSize) doUsage( "DiskSize<"<<(uint64_t)bufSize<<", we can't deal with that.");
+
 	double curSpeed, totSpeed = 0;
 	auto startT = std::chrono::steady_clock::now();
 	for(int i = 0; i < NUMPASSES; i++) {
-		if((curSpeed = doPass(fd,'a'+i,diskSize)) < 0) { cerr << "Failed a test" << endl; return -1; }
+		if((curSpeed = doPass(fd,'a'+i,diskSize,bufSize)) < 0) { cerr << "Failed a test" << endl; return -1; }
 		totSpeed += curSpeed;
 	}
 	close(fd);
