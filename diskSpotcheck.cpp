61118f57a147581ca48aa7e04232c2edbf0ba836
Andras Fekete <bandi13@users.noreply.github.com>
2016-06-30 23:21:59 -0400

    Added optional readOnly operation

--- diskSpotcheck.cpp
+++ diskSpotcheck.cpp
@@ -45,7 +45,7 @@ static void dropSystemCache() {
 
 #define LOCCNT 1000
 #define NUMPASSES 3
-double doPass(int fd, char c, uint64_t maxLoc, size_t bufSize) {
+double doPass(std::string &diskPath, char c, uint64_t maxLoc, size_t bufSize, bool readOnly) {
 	std::unique_ptr<char[]> raiiBuf = std::make_unique<char[]>(bufSize);
 	char *buf = raiiBuf.get();
 	uint64_t locs[LOCCNT];
@@ -53,21 +53,25 @@ double doPass(int fd, char c, uint64_t maxLoc, size_t bufSize) {
 	dropSystemCache();
 	srand(c);
 	for(uint64_t i = 0; i < bufSize; i++) buf[i] = rand();
-	cout << "Starting test of char=" << c << endl;
 	maxLoc -= bufSize; // make sure we don't accidentally try to write off the end of the file
 	locs[0] = 0; // make sure we get the beginning
 	locs[LOCCNT - 1] = maxLoc; // make sure we get the end
 	for(uint64_t i = 1; i < LOCCNT - 1; i++) {
 		locs[i] = (((float)rand() / RAND_MAX) * (maxLoc - locs[i-1] - bufSize)) / ((LOCCNT - 2)/4) + locs[i-1] + bufSize; // set up the location to be written relative to the last one
 	}
+	cout << "Starting test of char=" << c << endl;
 	auto startT = std::chrono::steady_clock::now();
-	for(uint64_t i = 0; i < LOCCNT; i++) {
-		//		cout << i << ": Writing to " << locs[i] << endl;
-		lseek64(fd,locs[i],SEEK_SET);
-		if(write(fd,buf,bufSize) != (int)bufSize) { cerr << "Didn't complete a write of " << bufSize << " * '" << c << "' at " << locs[i] << " because " << strerror(errno) << endl; return -1; }
+	int fd;
+	if((fd = open(diskPath.c_str(),O_RDWR|O_LARGEFILE)) == -1) return -1;
+	if(!readOnly) {
+		for(uint64_t i = 0; i < LOCCNT; i++) {
+			//		cout << i << ": Writing to " << locs[i] << endl;
+			lseek64(fd,locs[i],SEEK_SET);
+			if(write(fd,buf,bufSize) != (int)bufSize) { cerr << "Didn't complete a write of " << bufSize << " * '" << c << "' at " << locs[i] << " because " << strerror(errno) << endl; return -1; }
+		}
+		if(syncfs(fd) == -1) { cerr << "Sync error: " << strerror(errno) << endl; return -3; }
+		dropSystemCache();
 	}
-	if(syncfs(fd) == -1) { cerr << "Sync error: " << strerror(errno) << endl; return -3; }
-	dropSystemCache();
 	for(uint64_t i = 0; i < LOCCNT; i++) {
 		//		cout << i << ": Reading from " << locs[i] << endl;
 		lseek64(fd,locs[i],SEEK_SET);
@@ -86,52 +90,62 @@ double doPass(int fd, char c, uint64_t maxLoc, size_t bufSize) {
 				return -4;
 			}
 	}
+	close(fd);
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds >(std::chrono::steady_clock::now() - startT).count() / 1000.0;
 	double speed = ((double)(bufSize*LOCCNT)/duration)/(1024*1024);
 	cout << "Test completed in " << duration << " seconds. Speed= " << speed << " MB/s." << endl;
 	return speed;
 }
 
-#define doUsage(errStream) { cerr << errStream << endl << "Usage: " << argv[0] << " [-d <device=/dev/nbd0>] [-s <diskSizeInMB=auto>] [-b <bufSizeInKB=64>] [-h]" << endl; return -1; }
+#define doUsage(errStream) { cerr << errStream << endl << "Usage: " << argv[0] << " [-d <device=/dev/nbd0>] [-s <diskSizeInMB=auto>] [-b <bufSizeInKB=64>] [-h] [-r]" << endl; return -1; }
 int main(int argc, char *argv[]) {
-	int fd;
 	int opt;
+	bool readOnly = false;
 	size_t diskSize = 0;
 	size_t bufSize = 64*1024;
 	std::string diskPath = "/dev/nbd0";
-	while ((opt = getopt(argc, argv, "b:d:s:h")) != -1) {
+	while ((opt = getopt(argc, argv, "b:d:s:hr")) != -1) {
 		switch (opt) {
 			case 'b': bufSize = (size_t)atoi(optarg) * 1024; break;
 			case 's': diskSize = (size_t)atoi(optarg) * 1024 * 1024; break;
 			case 'd': diskPath = optarg; break;
+			case 'r': readOnly = true; break;
 			case 'h': doUsage("Help requested"); return -1;
 			default:  doUsage("Unknown argument"); return -1;
 		}
 	}
-	if((fd = open(diskPath.c_str(),O_RDWR|O_LARGEFILE)) == -1) doUsage("Error opening " << diskPath << ": " << strerror(errno));
-	if(diskSize == 0) {
-		struct stat fd_stat;
-		if(fstat(fd,&fd_stat)) doUsage("Error getting the stats on the file: " << strerror(errno));
-		if(S_ISBLK(fd_stat.st_mode)) ioctl(fd,BLKGETSIZE64, &diskSize);
-		else {
-			cout << "File stats: blksize=" << fd_stat.st_blksize << " blkcnt=" << fd_stat.st_blocks << endl;
-			diskSize = fd_stat.st_blksize * fd_stat.st_blocks;
+	{
+		int fd;
+		if((fd = open(diskPath.c_str(),O_RDWR|O_LARGEFILE)) == -1) doUsage("Error opening " << diskPath << ": " << strerror(errno));
+		if(diskSize == 0) {
+			struct stat fd_stat;
+			if(fstat(fd,&fd_stat)) doUsage("Error getting the stats on the file: " << strerror(errno));
+			if(S_ISBLK(fd_stat.st_mode)) ioctl(fd,BLKGETSIZE64, &diskSize);
+			else {
+				cout << "File stats: blksize=" << fd_stat.st_blksize << " blkcnt=" << fd_stat.st_blocks << endl;
+				diskSize = fd_stat.st_blksize * fd_stat.st_blocks;
+			}
 		}
+		close(fd);
 	}
 
 	cout << "Setting diskSize=" << diskSize << ", bufSize=" << bufSize << endl;
 
 	if(diskSize < bufSize) doUsage( "DiskSize<"<<(uint64_t)bufSize<<", we can't deal with that.");
 
-	cout << "Will be writing+reading " << bufSize * LOCCNT / (1024*1024) << "MB" << endl;
+	if(readOnly) cout << "Will be reading " << bufSize * LOCCNT / (1024*1024) << "MB" << endl;
+	else cout << "Will be writing+reading " << bufSize * LOCCNT / (1024*1024) << "MB" << endl;
 
 	double curSpeed, totSpeed = 0;
 	auto startT = std::chrono::steady_clock::now();
-	for(int i = 0; i < NUMPASSES; i++) {
-		if((curSpeed = doPass(fd,'a'+i,diskSize,bufSize)) < 0) { cerr << "Failed a test" << endl; return -1; }
-		totSpeed += curSpeed;
+	if(readOnly) {
+		if((totSpeed = doPass(diskPath,'a'+NUMPASSES-1,diskSize,bufSize,readOnly)) < 0) { cerr << "Failed a test" << endl; return -1; }
+	} else {
+		for(int i = 0; i < NUMPASSES; i++) {
+			if((curSpeed = doPass(diskPath,'a'+i,diskSize,bufSize,readOnly)) < 0) { cerr << "Failed a test" << endl; return -1; }
+			totSpeed += curSpeed;
+		}
 	}
-	close(fd);
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds >(std::chrono::steady_clock::now() - startT).count() / 1000.0;
 	cout << "All tests completed in " << duration << " seconds. Average speed=" << (totSpeed / NUMPASSES) << "MB/s." << endl;
 	return 0;
