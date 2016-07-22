681f8bf0e73869e578459bcec929af148f702f5d
Guanlai Li <guanlai@fusion.servebeer.com>
2016-07-22 12:08:49 -0400

    minor change about path and filename

--- resultPlot.py
+++ resultPlot.py
@@ -1,12 +1,25 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+#matplotlib.ioff()
 import matplotlib.pyplot as plt
 import csv
 import sys
+import os
 
-with open('/Users/guanlai/Desktop/testResult.csv', 'rb') as f:
+#pdf plot would be saved in a folder named resultPlot
+#if this folder is not exist, the script would genrate the directory
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'fileSystemPlot/')
+
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+
+
+with open(sys.argv[1], 'rb') as f:
     reader = csv.reader(f)
-    fs = sys.argv[1]
-    file = sys.argv[2]
+    fs = sys.argv[2]
+    file = sys.argv[3]
 #    print fs, file
     data = []
     xtick = ["w"]
@@ -25,5 +38,7 @@ plt.title('write/read test of '+file+' in '+fs)
 plt.ylabel('time (s)')
 plt.xlabel('index of w/r')
 plt.xticks(ind + width/2., xtick)
-plt.savefig(fs+"_"+file+"_test_result.pdf")
-plt.show()
+plt.autoscale(enable=True, axis='both', tight=None)
+#plt.savefig("/resultPlot/"+sys.argv[1][:-4]+"_"+fs+"_"+file+".pdf")
+plt.savefig(results_dir+sys.argv[1][17:-4]+"_"+fs+"_"+file+".pdf")
+#plt.show()
