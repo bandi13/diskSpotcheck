e4bb7f6f93fc1663223dec52b65c73cdf9518562
Andras Fekete <bandi13@users.noreply.github.com>
2016-08-07 22:02:51 -0400

    Made sure that the readback is always completed

--- filesystemTest.cpp
+++ filesystemTest.cpp
@@ -1,14 +1,7 @@
 //read and write test for RAIDX
-//Guanlai Li
-//liguanlai@gmail.com
-
 #include<iostream>
 #include<sstream>
 #include<fstream>
-#include<cstdlib>
-#include<cstdint>
-#include<string>
-#include<ctime>
 using namespace std;
 
 //usage syntax
@@ -24,47 +17,69 @@ void usage(char *progName) {
 //write test
 //currently I write n*1024 random char, where 'n' is a random number in range [0,1024]
 //future version would be take an argument for size, and take another argument for number of file to generate
-void write_test( const char* path ) {
+bool write_test( const char* path ) {
 	ofstream myFile;
 
 	for(uint16_t j = 0; j < NUM_FILES; j++ ) {
 		std::ostringstream fname;
 		fname << path << "/test" << j;
 		int maxSize = (rand()%1024)*1024;
-		srand(j);
 		myFile.open( fname.str(), std::ofstream::out | std::ofstream::trunc );
-		while(maxSize--) myFile << rand();
+		if(!myFile.is_open()) { cerr << "Can't open file for write: " << fname.str() << endl; return true; }
+		srand(j);
+		while(maxSize--) myFile << rand() << endl;
 		myFile.close();
 	}
+	cout << "Wrote " << NUM_FILES << " files" << endl;
+	return false;
 }
 
 //read test
 //current version can only support hard coded file name
 //future version would take an argument to read specific file
-void read_test(const char* path) {
+bool read_test(const char* path) {
 	ifstream myFile;
 	int i;
+	uint count;
 
 	for( int j = 0; j < NUM_FILES/4; j++ ) {
 		std::ostringstream fname;
 		uint16_t fileNum = rand() % NUM_FILES;
 		fname << path << "/test" << fileNum;
 
-		srand(fileNum);
 		myFile.open( fname.str() );
-		while (myFile >> i ) if(i != rand()) { cerr << "Invalid file written: " << fname.str() << endl; return; }
+		count = 0;
+		if(!myFile.is_open()) { cerr << "Can't open file for read: " << fname.str() << endl; return true; }
+		int newNum;
+		srand(fileNum);
+		while (myFile >> i) {
+			newNum = rand();
+			if(i != newNum) { cerr << "Invalid file written: " << fname.str() << '(' << i << "!=" << newNum << ") at " << count << "th value" << endl; return true; } else count++;
+		}
 		myFile.close();
+		cout << "Read(" << fname.str() << ") " << count << " values" << endl;
 	}
+	return false;
 }
 
 int main( int argc, char* argv[] ) {
 	if(argc != 3) { usage(argv[0]); return 1; }
+	bool doRead = false, doWrite = false;
 
-	if(string(argv[1]) == "r") read_test(argv[argc-1]);
-	else if(string(argv[1]) == "w") write_test(argv[argc-1]);
-	else if(string(argv[1]) == "a") { write_test(argv[argc-1]); read_test(argv[argc-1]); }
+	if(string(argv[1]) == "r") doRead = true;
+	else if(string(argv[1]) == "w") doWrite = true;
+	else if(string(argv[1]) == "a") { doRead = true; doWrite = true; }
 	else { usage(argv[0]); return 1; }
 
+	if(doWrite) {
+		if(write_test(argv[argc-1])) { cerr << "Failed write test" << endl; return 1; }
+	}
+
+	if(doRead) {
+		if(read_test(argv[argc-1])) { cerr << "Failed read test" << endl; return 1; }
+	}
+
+	cout << "Test completed successfully" << endl;
 	return 0;
 }
 
