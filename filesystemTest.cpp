499ca6cef1b53c284e3aad63a017f2d93b8b847d
Andras Fekete <bandi13@users.noreply.github.com>
2016-09-16 13:59:29 -0400

    Undo accidental commit of my changes to this file.

--- filesystemTest.cpp
+++ filesystemTest.cpp
@@ -1,76 +1,85 @@
 //read and write test for RAIDX
-//Guanlai Li
-//liguanlai@gmail.com
-
 #include<iostream>
 #include<sstream>
 #include<fstream>
-#include<cstdlib>
-#include<cstdint>
-#include<string>
-#include<ctime>
-#include <algorithm>
-#include <fcntl.h>
-#include <unistd.h>
-#include<string.h>
-#include<memory>
 using namespace std;
 
-#define NUMBUFFERS 10
+//usage syntax
+void usage(char *progName) {
+	cout << "Usage: " << progName << " <r|w|a> <testFilePath>" << endl;
+	cout << "'r' for read test" << endl;
+	cout << "'w' for write test" << endl;
+	cout << "'a' for both" << endl;
+}
+
+#define NUM_FILES 100
+
+//write test
+//currently I write n*1024 random char, where 'n' is a random number in range [0,1024]
+//future version would be take an argument for size, and take another argument for number of file to generate
+bool write_test( const char* path ) {
+	ofstream myFile;
+
+	for(uint16_t j = 0; j < NUM_FILES; j++ ) {
+		std::ostringstream fname;
+		fname << path << "/test" << j;
+		int maxSize = (rand()%1024)*1024;
+		myFile.open( fname.str(), std::ofstream::out | std::ofstream::trunc );
+		if(!myFile.is_open()) { cerr << "Can't open file for write: " << fname.str() << endl; return true; }
+		srand(j);
+		while(maxSize--) myFile << rand() << endl;
+		myFile.close();
+	}
+	cout << "Wrote " << NUM_FILES << " files" << endl;
+	return false;
+}
+
+//read test
+//current version can only support hard coded file name
+//future version would take an argument to read specific file
+bool read_test(const char* path) {
+	ifstream myFile;
+	int i;
+	uint count;
+
+	for( int j = 0; j < NUM_FILES/4; j++ ) {
+		std::ostringstream fname;
+		uint16_t fileNum = rand() % NUM_FILES;
+		fname << path << "/test" << fileNum;
+
+		myFile.open( fname.str() );
+		count = 0;
+		if(!myFile.is_open()) { cerr << "Can't open file for read: " << fname.str() << endl; return true; }
+		int newNum;
+		srand(fileNum);
+		while (myFile >> i) {
+			newNum = rand();
+			if(i != newNum) { cerr << "Invalid file written: " << fname.str() << '(' << i << "!=" << newNum << ") at " << count << "th value" << endl; return true; } else count++;
+		}
+		myFile.close();
+		cout << "Read(" << fname.str() << ") " << count << " values" << endl;
+	}
+	return false;
+}
 
 int main( int argc, char* argv[] ) {
-    long size = atoi(argv[1]);
-    
-    vector<unique_ptr<char>>base;
-    for(int i = 0; i < NUMBUFFERS; i++) {
-        srand(i);
-				char *testStr;
-				if(posix_memalign((void **)&testStr, 4096, 4096)) { cerr << "Failed aligning memory" << strerror(errno) << endl; return 1; }
-        for(int j = 0; j < 4096; j++) ((char *)testStr)[j] = 'a' + rand()%26;
-				base.emplace_back(unique_ptr<char>(testStr));
-    }
+	if(argc != 3) { usage(argv[0]); return 1; }
+	bool doRead = false, doWrite = false;
 
-    clock_t t;
-    float time;
-    t = clock();
-    
-    int fd = open("test", O_RDWR | O_CREAT | O_LARGEFILE | O_DIRECT | O_SYNC | O_TRUNC, S_IWUSR | S_IRUSR);
-    if(fd<0){ cerr << "error when open file: " << strerror(errno) << endl; return 1;}
-    
-    for(int i = 0; i < size; i++) {
-        srand(i);
-        for(int j = 0; j < 1024; j++) {
-            int wt = write(fd,base[rand()%NUMBUFFERS].get(), 4096);
-            if(wt != 4096) { cerr << "error when write file " << wt << " " << strerror(errno) << endl; return 1; }
-        }
-    }
+	if(string(argv[1]) == "r") doRead = true;
+	else if(string(argv[1]) == "w") doWrite = true;
+	else if(string(argv[1]) == "a") { doRead = true; doWrite = true; }
+	else { usage(argv[0]); return 1; }
 
-    cout << "success write " << size*4 << " MB file" << endl;
-    if(close(fd)<0) {cerr << "error when close file after write" << endl; return 1;}
-    t = clock() - t;
-    time = (float)t/CLOCKS_PER_SEC;
-    cout << "write speed " << size*4/time << " MB/s" << endl;
-    
-    t = clock();
-    fd = open("test", O_RDWR | O_CREAT | O_LARGEFILE | O_DIRECT, S_IWUSR | S_IRUSR);
-    if(fd<0){ cerr << "error when open file: " << strerror(errno) << endl; return 1;}
-		void *testStr;
-		if(posix_memalign(&testStr, 4096, 4096)) { cerr << "Failed aligning memory" << strerror(errno) << endl; return 1; }
-    int wt = read(fd,testStr, 4096);
-    cout << wt << " " << string((char *) testStr) << endl;
-    for(int i = 0; i < size; i++) {
-        srand(i);
-        for(int j = 0; j < 1024; j++) {
-            cout << "i="<< i << ", j=" << j << endl;
-            if(strncmp((char *)testStr, (char *)base[rand()%NUMBUFFERS].get(),4096)!=0){cerr << "error validate" << endl; return 1;}
-        }
-    }
-    cout << "success validation" << endl;
-    if(close(fd)<0) {cerr << "error when close file after read" << endl; return 1;}
-    t = clock() - t;
-    time = (float)t/CLOCKS_PER_SEC;
-    cout << "read in " << time << " seconds" << endl;
+	if(doWrite) {
+		if(write_test(argv[argc-1])) { cerr << "Failed write test" << endl; return 1; }
+	}
 
-    return 0;
+	if(doRead) {
+		if(read_test(argv[argc-1])) { cerr << "Failed read test" << endl; return 1; }
+	}
+
+	cout << "Test completed successfully" << endl;
+	return 0;
 }
 
